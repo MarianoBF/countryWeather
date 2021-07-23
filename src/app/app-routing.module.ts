02102509a849193d531f5489a31ddaeb5c164e8b
Mariano Fernandez <mfernandez@balanz.com>
2021-07-23 14:03:57 -0300

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CountriesComponent } from './components/countries/countries.component';
-import { CountryComponent } from './components/country/country.component';
+import { CountryComponent } from './countries/pages/country/country.component';
+import { CapitalComponent } from './countries/pages/capital/capital.component';
+import { CountryDetailComponent } from './countries/pages/country-detail/country-detail.component';
+import { RegionComponent } from './countries/pages/region/region.component';
 
 const routes: Routes = [
-  {path: "", component: CountriesComponent},
-  {path: "pais/:country_code", component: CountryComponent},
+  {path: "", component: CountryComponent, pathMatch: 'full'},
+  {path: "region", component: RegionComponent, pathMatch: 'full'},
+  {path: "capital", component: CapitalComponent, pathMatch: 'full'},
+  {path: "pais/:id", component: CountryDetailComponent, pathMatch: 'full'},
   {path: "**", redirectTo: "/"}
 ];
 
