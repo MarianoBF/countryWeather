f94b9b581ad063f270d5378b2b71e1c30864e47e
Mariano <mariano.b.fernandez@gmail.com>
2021-08-25 09:35:32 -0300

    move files & delete unused

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HttpClientModule } from "@angular/common/http"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { LoadingComponent } from './components/loading/loading.component';
+import { LoadingComponent } from './shared/loading/loading.component';
 import { CountriesModule } from './countries/countries.module';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
